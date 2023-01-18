7ed168709d589213f5f2a4dd65a5f24823031120
ZhaoCe <17631807110@163.com>
2023-01-18 15:38:12 +0800

    refactor: 代码重构&问题修复

--- Vite-SSR/server.ts
+++ Vite-SSR/server.ts
@@ -1,53 +1,28 @@
 import fs from "fs";
 import path from "path";
-import { fileURLToPath } from "url";
 import express from "express";
+import { fileURLToPath } from "url";
 import { createServer as createViteServer, ViteDevServer } from "vite";
-import { renderToString } from "@vue/server-renderer";
 
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
+type Path = string;
+
 const isProd = process.env.NODE_ENV === "production";
-/* TODO 重构 & 预请求 & 增量构建 */
-
+/* TODO 预请求 & 增量构建 */
 async function createServer() {
   const app = express();
 
-  let vite: ViteDevServer | null = null;
-  if (!isProd) {
-    // 以中间件模式创建 Vite 应用，这将禁用 Vite 自身的 HTML 服务逻辑
-    // 并让上级服务器接管控制
-    vite = await createViteServer({
-      server: { middlewareMode: true },
-      appType: "custom"
-    });
-
-    // 使用 vite 的 Connect 实例作为中间件
-    // 如果你使用了自己的 express 路由（express.Router()），你应该使用 router.use
-    app.use(vite.middlewares);
-  } else {
-    app.use((await import("compression")).default());
-    app.use(
-      "/",
-      (await import("serve-static")).default(
-        path.resolve(__dirname, "dist/client"),
-        {
-          index: false
-        }
-      )
-    );
-  }
+  // 使用中间件
+  const vite = await useMiddleware(app);
 
   // 服务 index.html
   app.use("*", async (req, res) => {
-    const url = req.originalUrl;
     try {
+      const url = req.originalUrl;
+
       let template, render;
       if (!isProd) {
         // 1. 读取 index.html
-        template = fs.readFileSync(
-          path.resolve(__dirname, "index.html"),
-          "utf-8"
-        );
+        template = fs.readFileSync(resolve("index.html"), "utf-8");
         // 2. 应用 Vite HTML 转换。这将会注入 Vite HMR 客户端，
         //    同时也会从 Vite 插件应用 HTML 转换。
         //    例如：@vitejs/plugin-react 中的 global preambles
@@ -57,16 +32,12 @@ async function createServer() {
         //    并提供类似 HMR 的根据情况随时失效。
         render = (await vite!.ssrLoadModule("/src/entry-server.ts")).render;
       } else {
-        template = fs.readFileSync(
-          path.resolve(__dirname, "dist/client/index.html"),
-          "utf-8"
-        );
+        template = fs.readFileSync(resolve("dist/client/index.html"), "utf-8");
         render = (await import("./dist/server/entry-server.js")).render;
       }
       // 4. 渲染应用的 HTML。这假设 entry-server.js 导出的 `render`
       //    函数调用了适当的 SSR 框架 API。
-      //    例如 ReactDOMServer.renderToString()
-      const appHtml = await renderToString(render());
+      const appHtml = await render();
       // 5. 注入渲染后的应用程序 HTML 到模板中。
       const html = template.replace(`<!-- ssr-app -->`, appHtml);
       // 6. 返回渲染后的 HTML。
@@ -84,6 +55,44 @@ async function createServer() {
     console.log("Node 服务器已启动~");
     console.log("http://localhost:3001");
   });
+}
+
+async function useMiddleware(app) {
+  let vite: ViteDevServer | null = null;
+  if (!isProd) {
+    // 以中间件模式创建 Vite 应用，这将禁用 Vite 自身的 HTML 服务逻辑
+    // 并让上级服务器接管控制
+    vite = await createViteServer({
+      root: process.cwd(),
+      server: {
+        middlewareMode: true,
+        watch: {
+          // During tests we edit the files too fast and sometimes chokidar
+          // misses change events, so enforce polling for consistency
+          usePolling: true,
+          interval: 100
+        }
+      },
+      appType: "custom"
+    });
+
+    // 使用 vite 的 Connect 实例作为中间件
+    // 如果你使用了自己的 express 路由（express.Router()），你应该使用 router.use
+    app.use(vite.middlewares);
+  } else {
+    app.use((await import("compression")).default());
+    app.use(
+      (await import("serve-static")).default(resolve("dist/client"), {
+        index: false
+      })
+    );
+  }
+  return vite;
+}
+
+function resolve(p: Path): Path {
+  const __dirname = path.dirname(fileURLToPath(import.meta.url));
+  return path.resolve(__dirname, p);
 }
 
 createServer();
--- Vite-SSR/src/App.vue
+++ Vite-SSR/src/App.vue
@@ -1,17 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
+  <div class="container">
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
     <a href="https://vuejs.org/" target="_blank">
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
+    <div class="text">SSR APP</div>
   </div>
 </template>
 
 <style scoped>
+.container {
+  text-align: center;
+  margin-top: 100px;
+}
 .logo {
   height: 6em;
   padding: 1.5em;
@@ -22,5 +27,10 @@
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+.text {
+  font-size: 30px;
+  margin-top: 30px;
+  color: cornflowerblue;
 }
 </style>
